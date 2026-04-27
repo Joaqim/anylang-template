cb2afee424df224c4554253ec9cb16c814289e99
Joaqim Planstedt <mail@joaqim.xyz>
2026-04-27 11:49:08 +0200

    feat(flake): use npins for nixpkgs flake input

--- flake.nix
+++ flake.nix
@@ -1,47 +1,57 @@
+# IMPORTANT: This flake intentionally has ZERO inputs.
+#
+# nixpkgs is imported via fetchTarball in nix/nixpkgs.nix, bypassing the
+# flake input system. This is critical for `nix develop` performance.
+#
+# DO NOT add flake inputs (nixpkgs, flake-parts, git-hooks, etc.).
+# Instead, use fetchTarball or callPackage in nix/ files.
 {
-  inputs = {
-    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
-  };
-
   outputs =
     {
       nixpkgs,
       ...
     }:
     let
-      inherit (nixpkgs) lib;
-      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      eachSystem =
+        f:
+        builtins.listToAttrs (
+          map (system: {
+            name = system;
+            value = f (import ./nix/nixpkgs.nix { inherit system; });
+          }) systems
+        );
+
     in
     {
-      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShellNoCC {
-            # Set NIX_PATH for nixd inlay hints
-            env.NIX_PATH = "nixpkgs=${nixpkgs.outPath}";
+      formatter = eachSystem (pkgs: pkgs.nixfmt-tree);
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShellNoCC {
+          # Set NIX_PATH for nixd inlay hints
+          env.NIX_PATH = "nixpkgs=${nixpkgs.outPath}";
 
-            buildInputs = (
-              with pkgs;
-              [
-                # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
-                bashInteractive
-                findutils # xargs
-                nixfmt # nixfmt-rfc-style is now nixfmt: https://github.com/NixOS/nixpkgs/pull/425068
-                nixfmt-tree
-                nixd
-                go-task
+          buildInputs = (
+            with pkgs;
+            [
+              # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
+              bashInteractive
+              findutils # xargs
+              nixfmt # nixfmt-rfc-style is now nixfmt: https://github.com/NixOS/nixpkgs/pull/425068
+              nixfmt-tree
+              nixd
+              go-task
+              npins
 
-                dprint
-                typos
-                zizmor
-              ]
-            );
-          };
-        }
-      );
+              dprint
+              typos
+              zizmor
+            ]
+          );
+        };
+      });
     };
 }
---
+++ nix/overlay.nix
@@ -0,0 +1,4 @@
+# Add custom packages and overrides here
+_final: _prev: {
+  #my-package = final.callPackage ./packages/my-package { };
+}
